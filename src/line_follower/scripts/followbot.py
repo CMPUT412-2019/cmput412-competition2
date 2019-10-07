1053f663ccaf429c609d1fe86604e2a5d20c63ff
Alex Epp <aepp2@ualberta.ca>
2019-10-07 15:26:18 -0600

    Add line-following from demo 3

---
+++ src/line_follower/scripts/followbot.py
@@ -0,0 +1,39 @@
+import rospy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
+from smach import State, StateMachine
+from smach_ros import IntrospectionServer
+import cv2
+import cv_bridge
+import numpy as np
+import time
+
+from state.linefollow import LineFollowState, FilterStopBehavior
+from state.stopped import StoppedState
+
+
+def make_realcourse_follow_state():
+    forward_speed = 0.8
+    line_filter = lambda hsv: cv2.inRange(hsv, np.array([0,  0,  200]), np.array([255, 50, 255]))
+    stop_filter = (lambda hsv: cv2.inRange(hsv, np.asarray([0, 70, 50]), np.asarray([10, 255, 250])) |
+                                cv2.inRange(hsv, np.asarray([170, 70, 50]), np.asarray([180, 255, 250])))
+
+    return LineFollowState(
+        forward_speed=forward_speed,
+        line_filter=line_filter,
+        stop_detector=FilterStopBehavior(stop_filter=stop_filter)
+    )
+
+
+rospy.init_node('follower')
+
+sm = StateMachine(outcomes=['ok', 'stop'])
+with sm:
+    StateMachine.add('FOLLOW', make_realcourse_follow_state(), transitions={'stop': 'STOP'})
+    StateMachine.add('STOP', StoppedState(), transitions={'ok': 'FOLLOW'})
+
+sis = IntrospectionServer('smach_server', sm, '/SM_ROOT')
+sis.start()
+
+sm.execute()
---
+++ src/line_follower/scripts/state/linefollow.py
@@ -0,0 +1,120 @@
+import rospy
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64
+from smach import State
+import cv2
+import cv_bridge
+import numpy as np
+
+
+class StopBehavior:
+    def tick(self, hsv):
+        pass
+
+
+class FilterStopBehavior:
+    def __init__(self, stop_filter):
+        self.saw_stopline = False
+        self.stop_filter = stop_filter
+
+    def tick(self, hsv):
+        stop_mask = self.get_eye_mask(hsv) & self.stop_filter(hsv)
+
+        stop_mask_empty = np.sum(stop_mask) < 300
+        if self.saw_stopline and stop_mask_empty:
+            return False
+        else:
+            self.saw_stopline = not stop_mask_empty
+
+        cv2.imshow('stop_mask', stop_mask * 255)
+        return True
+
+    @staticmethod
+    def get_eye_mask(hsv):
+        h, w, d = hsv.shape
+        search_top = int(1.5 * h / 4 - 20)
+        search_bot = int(1.5 * h / 4)
+        eye_mask = np.ones((h, w), dtype=bool)
+        eye_mask[0:search_top, 0:w] = False
+        eye_mask[search_bot:h, 0:w] = False
+        return eye_mask
+
+
+class LineFollowState(State):
+    class StopError(Exception):
+        pass
+
+    def __init__(self, forward_speed, line_filter, stop_detector):
+        State.__init__(self, outcomes=['ok', 'stop'])
+
+        self.forward_speed = forward_speed
+        self.line_filter = line_filter
+
+        self.stop_detector = stop_detector  # type: StopBehavior
+
+        self.bridge = cv_bridge.CvBridge()
+        self.twist = Twist()
+        self.image_sub = rospy.Subscriber('usb_cam_node/image_raw', Image, self.image_callback)
+        self.angle_error_pub = rospy.Publisher('angle_error', Float64, queue_size=1)
+        self.linear_error_pub = rospy.Publisher('linear_error', Float64, queue_size=1)
+
+        self.rate = rospy.Rate(10)
+        self.image = None
+
+    def image_callback(self, msg):
+        self.image = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+    def wait_for_image(self):
+        while self.image is None:
+            self.rate.sleep()
+
+    def execute(self, ud):
+        self.wait_for_image()
+
+        try:
+            while True:
+                self.tick()
+                self.rate.sleep()
+        except self.StopError:
+            return 'stop'
+
+    def tick(self):
+        image = np.copy(self.image)
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+        eye_mask = self.get_eye_mask(hsv)
+        line_mask = self.line_filter(hsv) & eye_mask
+
+        # Check for stopping condition
+        if not self.stop_detector.tick(hsv):
+            raise self.StopError()
+
+        # Line following
+        if not self.is_mask_empty(line_mask):
+            cx, cy = self.get_mask_center(line_mask)
+            cv2.circle(image, (cx, cy), 20, (0, 0, 255), -1)
+            err = (cx - image.shape[1] / 2) / float(image.shape[1])
+
+            self.linear_error_pub.publish(-self.forward_speed * (1. - abs(err)))
+            self.angle_error_pub.publish(err)
+
+        # Display
+        cv2.imshow('main_camera', image)
+        cv2.imshow('line_mask',line_mask * 255)
+        cv2.waitKey(3)
+
+    @staticmethod
+    def get_eye_mask(image):
+        return np.ones(image.shape[:2]).astype('uint8')
+
+    @staticmethod
+    def get_mask_center(mask):
+        M = cv2.moments(mask)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        return cx, cy
+
+    @staticmethod
+    def is_mask_empty(mask):
+        return np.all(mask == 0)
